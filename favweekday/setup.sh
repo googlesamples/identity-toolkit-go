8fbbc703087c7f8076448a7206dd0be6f90a1385
Mengcheng Duan <mengcheng@google.com>
2014-10-13 16:36:38 -0700

    Update the setup script.

--- favweekday/setup.sh
+++ favweekday/setup.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-mkdir -p github.com/gorilla
-cd github.com/gorilla
-git clone https://github.com/gorilla/context.git
-git clone https://github.com/gorilla/mux.git
-git clone https://github.com/gorilla/securecookie.git
-git clone https://github.com/gorilla/sessions.git
-cd -
-
-mkdir -p github.com/google
-cd github.com/google
-git clone https://github.com/google/identity-toolkit-go-client.git
-cd -
-
-mkdir -p code.google.com/p
-cd code.google.com/p
-hg clone https://code.google.com/p/xsrftoken/
-cd -
+go get github.com/gorilla/mux
+go get github.com/gorilla/sessions
+go get github.com/google/identity-toolkit-go-client/gitkit
+go get code.google.com/p/xsrftoken
